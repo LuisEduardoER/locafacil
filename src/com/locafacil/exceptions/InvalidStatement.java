3cc6e39c4f4a1c0a12740cc7a5b7f68ca2c68c0c
danielgregorio <danielgregorio@gmail.com>
2008-09-17 22:32:33 +0000

    Classes de validação incluidas no pacote com.locafacil.util (para validação de CPF, cartao de credito, email)
    classes de exceção criadas e incluidas no pacote com.locafacil.exceptions
    
    classes relativas ao aluguel criadas e adicionadas no com.locafacil.common
    
    pacote com.locafacil.gui criado, nele deverão ser criados os forms

---
+++ src/com/locafacil/exceptions/InvalidStatement.java
@@ -0,0 +1,20 @@
+package com.locafacil.exceptions;
+
+public class InvalidStatement extends Exception {
+
+	public InvalidStatement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidStatement(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidStatement(String cpf, String mensagem) {
+		super(mensagem);
+		// TODO Auto-generated constructor stub
+	}
+
+}
