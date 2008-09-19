83ef343b2e4d0bbd7321544e77735ddf4404f4b3
danielgregorio <danielgregorio@gmail.com>
2008-09-19 20:37:58 +0000

    integração com SWINGML
    verificar dependencias
    
    demo 
    http://swingml.sourceforge.net/demo.html

--- src/com/locafacil/Starter.java
+++ src/com/locafacil/Starter.java
@@ -1,5 +1,8 @@
 package com.locafacil;
 
+import org.swingml.SwingMLInitializationError;
+import org.swingml.SwingMLRenderer;
+
 public class Starter {
 
 	/**
@@ -7,7 +10,16 @@ public class Starter {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] params = new String[3];
+		params[0] = "file:///J:/Documents%20and%20Settings/Administrador/workspace/LocaFacil/src/com/locafacil/gui/formTeste.xml";
+		params[1] = "400";
+		params[2] = "400";
+		try {
+			SwingMLRenderer.main(params);
+		} catch (SwingMLInitializationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 //Teste de Washington
 }
