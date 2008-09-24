c152d5737474d37689ea2ed35a1e2e129689a35f
hildannydantas <hildannydantas@hotmail.com>
2008-09-24 00:48:26 +0000

    Inclusão do JAR de configuração de Look and Feel Conclusão do formulário genérico de pesquisa

--- src/com/locafacil/Starter.java
+++ src/com/locafacil/Starter.java
@@ -1,5 +1,9 @@
 package com.locafacil;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
 import com.locafacil.gui.FormSearch;
 
 public class Starter {
@@ -8,9 +12,16 @@ public class Starter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//MetalLookAndFeel.setCurrentTheme(new OrangeTheme());
+		try {
+			UIManager.setLookAndFeel( new com.nilo.plaf.nimrod.NimRODLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		FormSearch fm = new FormSearch();
-		fm.show();
+		//fm.pack();
+		fm.setVisible(true);
 	}
 //Teste de Washington
 }
