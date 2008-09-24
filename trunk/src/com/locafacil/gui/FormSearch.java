06dc07cff9afe06133e8228283d231a18fcde63e
hildannydantas@hotmail.com <hildannydantas@hotmail.com@2167b220-8379-11dd-ac5a-f353d88c4ef8>
2008-09-24 00:48:26 +0000

    Inclusão do JAR de configuração de Look and Feel Conclusão do formulário genérico de pesquisa

--- trunk/src/com/locafacil/gui/FormSearch.java
+++ trunk/src/com/locafacil/gui/FormSearch.java
@@ -3,12 +3,15 @@ package com.locafacil.gui;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class FormSearch extends JFrame {
 
@@ -22,16 +25,21 @@ public class FormSearch extends JFrame {
 	private JPanel panelButtons;
 	private JPanel panelHolder;
 	private JScrollPane panelTable;
+	private DefaultTableModel tableModel;
+	private JTable formTable;
 	
+	private JButton btOpen;
+	private JButton btEdit;
+	private JButton btClose;
 	
 	public FormSearch(){
 		initializeForm();
 		initializeControls();
+		this.setSize(500,450);
 	}
 	
 	private void initializeForm(){
-		this.setTitle("Pesquisa");
-		this.setSize(400,500);
+		this.setTitle("Pesquisa");		
 		this.getContentPane().setLayout(new BorderLayout());
 	}
 	
@@ -41,20 +49,36 @@ public class FormSearch extends JFrame {
 		txFilterValue = new JTextField();
 		lbFilterField = new JLabel("Campo para pesquisa");
 		cbFilterField = new JComboBox();
-		panelControls.setLayout(null);
+		
+		panelControls.setLayout(new GridLayout(3,2));
 		panelControls.add(lbFilterValue);
 		panelControls.add(txFilterValue);
 		panelControls.add(lbFilterField);
 		panelControls.add(cbFilterField);
 		panelButtons = new JPanel();
-		panelTable = new JScrollPane();
-		panelTable.add(panelControls);
+		
+		this.tableModel = new DefaultTableModel();
+		this.tableModel.addColumn("a");
+		this.tableModel.addColumn("b");
+		this.tableModel.addColumn("c");
+		this.formTable = new JTable( this.tableModel );
+		panelTable = new JScrollPane(formTable);
+		
+		
 		panelHolder = new JPanel();
 		panelHolder.setLayout(new GridLayout(1,2));
-		panelHolder.add(panelControls);
+		//panelHolder.add(panelControls);
 		panelHolder.add(panelTable);
 		
+		btOpen = new JButton("Novo");
+		btEdit = new JButton("Editar");
+		btClose = new JButton("Fechar");
 		
+		panelButtons.add(btOpen);
+		panelButtons.add(btEdit);
+		panelButtons.add(btClose);
+		
+		this.getContentPane().add(panelControls, BorderLayout.NORTH);
 		this.getContentPane().add(panelHolder, BorderLayout.CENTER);
 		this.getContentPane().add(panelButtons, BorderLayout.SOUTH);
 	}
