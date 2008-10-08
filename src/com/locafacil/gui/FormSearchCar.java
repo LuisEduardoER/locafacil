f92970c039af209eb96ed3594dd68af6c147d8c8
uoxitosoares <uoxitosoares@2167b220-8379-11dd-ac5a-f353d88c4ef8>
2008-10-08 14:14:52 +0000

    Foi adicionado o método da ação do botão editar

--- src/com/locafacil/gui/FormSearchCar.java
+++ src/com/locafacil/gui/FormSearchCar.java
@@ -3,12 +3,16 @@ package com.locafacil.gui;
 import java.util.Iterator;
 import java.util.Vector;
 
+import javax.swing.JOptionPane;
+
 import com.locafacil.Starter;
 import com.locafacil.common.Car;
 import com.locafacil.common.Client;
 import com.locafacil.common.FieldType;
 
 public class FormSearchCar extends FormSearch {
+	public static FormCadastroCarro fm;
+	
 	static String formTitle = "Pesquisa de Carros";
 	static String[] fieldsDef = {"vc_placa", "vc_chassi", "in_ano","in_cod_modelo"};
 	static String[] fields = {"Placa ", "Chassis", "Ano", "Modelo"}; // do combobox
@@ -19,11 +23,48 @@ public class FormSearchCar extends FormSearch {
 		super(formTitle,fields,cols,tipos);
 	}
 	
+	
 	protected void actionBtPesquisar(){
 		Vector<Car> rs = Starter.db.getCars();
 		Iterator<Car> it = rs.iterator();
 		while(it.hasNext()){
 			tableModel.addRow(it.next().getRowLine());
+		}
+	}
+	
+	public static FormCadastroCarro getFm(){
+		if(fm==null){
+			fm = new FormCadastroCarro();
+			fm.setModal(true);
+		}
+		return fm;
+	}
+	
+	/*MÉTODO QUE VERIFICAR SE FOI SELECIONADO ALGUM CLIENTE*/
+	public int getLinhaSelecionada(){
+		int result;
+		result = (formTable.getSelectedRow() != -1) ? formTable.getSelectedRow() : -1;
+		return result;
+	}
+	
+	/*AÇÃO DO BOTÃO EDITAR EM BUSCAR CARROS*/
+	protected void actionBtEdit(){
+		getFm().estado = FormCadastro.EDITAR;
+		try {
+			if (getLinhaSelecionada() != -1){
+				Car ca = (Car)FormSearch.vetor.elementAt(getLinhaSelecionada());
+				getFm().setCar(ca);
+				getFm().setVisible(true);
+				actionBtPesquisar();	
+				
+			}else{
+				JOptionPane.showMessageDialog(null, "Selecione um carro !");
+			}
+			
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			System.out.println("fudeu ");
+			e.printStackTrace();
 		}
 	}
 	
